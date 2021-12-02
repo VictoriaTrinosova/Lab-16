dca0de745ad3824e72e7fa185742647e86886540
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-12-02 21:08:45 +0300

    Добавьте файлы проекта.

---
+++ Lab 16/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
+
+namespace Lab_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] product = new Product[5];
+            product[0] = new Product();
+            product[1] = new Product();
+            product[2] = new Product();
+            product[3] = new Product();
+            product[4] = new Product();
+            foreach (var i in product)
+            {
+                i.String();
+            }
+
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented=true
+            };
+            string jsonString1 = JsonSerializer.Serialize(product, options);
+            Console.WriteLine(jsonString1);
+            StreamWriter file = new StreamWriter("C:/Users/Виктория/Desktop/Bim проектирование/лр 16/Products.json");
+            file.Write(jsonString1);
+            file.Close();
+            Console.ReadKey();
+        }
+    }
+
+    class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public void String()
+        {
+            Code = Convert.ToInt32(Console.ReadLine());
+            Name = Console.ReadLine();
+            Price = Convert.ToDouble(Console.ReadLine());
+        }
+
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lab16.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Lab16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("C:/Users/Виктория/Desktop/Bim проектирование/лр 16/Products.json");
+            string jsonString = file.ReadToEnd();
+            Console.WriteLine(jsonString);
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+            double max = 0;
+            string Name = "";
+            foreach (var prod in products)
+            {
+                if(prod.Price>max)
+                {
+                    max = prod.Price;
+                    Name = prod.Name;
+                }
+            }
+            Console.WriteLine("Название товара с наибольшей ценой: {0}",Name);
+            Console.WriteLine("Наибольшая цена: {0}",max);
+            Console.ReadKey();
+
+        }
+    }
+    class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        }
+    }
+    
+
+
